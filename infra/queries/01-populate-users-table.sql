248ce6fdf2954132881fa07c9d1a3019a89828c6
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-06-04 23:54:48 -0300

    feat: script to populate `users` table

---
+++ infra/queries/01-populate-users-table.sql
@@ -0,0 +1,24 @@
+INSERT INTO users
+	(
+    email,
+    name,
+    user_name,
+    user_role,
+    password
+  )
+VALUES
+	(
+    'janedoe@email.com',
+    'Jane Doe',
+    'janeDoe',
+    'USER',
+    '$2b$10$Vtq78yo4RxbWPtARY6R7CO2vVeGu1Nd4XASIhxB/PylfTL1/Bf3TG'
+  ),
+  (
+    'gabriel@email.com',
+    'Gabriel',
+    'gbsouza',
+    'ADMIN',
+    '$2b$10$Vtq78yo4RxbWPtARY6R7CO2vVeGu1Nd4XASIhxB/PylfTL1/Bf3TG'
+  );
+
