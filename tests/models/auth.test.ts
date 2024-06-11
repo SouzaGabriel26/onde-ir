e88e995f23ee927e873db13840f26832ce45f8d5
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-06-11 20:29:34 -0300

    feat(`models/authentication`): implement `SignIn` functionality

--- tests/models/auth.test.ts
+++ tests/models/auth.test.ts
@@ -1,6 +1,7 @@
 import { createAuthenticationDataSource } from '../../data/authentication';
 import { database } from '../../infra/database';
-import { auth, SignUpProps } from '../../models/authentication';
+import { auth } from '../../models/authentication';
+import { SignInProps, SignUpProps } from '../../types';
 import { orchestrator } from '../orchestrator';
 
 beforeAll(async () => {
@@ -220,6 +221,106 @@ describe('> models/authentication', () => {
           fields: ['userName'],
         },
       });
+    });
+  });
+
+  describe('Invoking "signIn" method', () => {
+    test('Providing an empty object', async () => {
+      const authDataSource = createAuthenticationDataSource();
+
+      const result = await auth.signIn(authDataSource, {} as SignInProps);
+
+      expect(result).toStrictEqual({
+        data: null,
+        error: {
+          message: 'O e-mail é obrigatório',
+          fields: ['email'],
+        },
+      });
+    });
+
+    test('Providing no "email" property', async () => {
+      const authDataSource = createAuthenticationDataSource();
+
+      const result = await auth.signIn(authDataSource, {
+        password: '123456',
+      } as SignInProps);
+
+      expect(result).toStrictEqual({
+        data: null,
+        error: {
+          message: 'O e-mail é obrigatório',
+          fields: ['email'],
+        },
+      });
+    });
+
+    test('Providing no "password" property', async () => {
+      const authDataSource = createAuthenticationDataSource();
+
+      const result = await auth.signIn(authDataSource, {
+        email: 'gabriel@email.com',
+      } as SignInProps);
+
+      expect(result).toStrictEqual({
+        data: null,
+        error: {
+          message: 'A senha é obrigatória',
+          fields: ['password'],
+        },
+      });
+    });
+
+    test('Providing invalid "email"', async () => {
+      const authDataSource = createAuthenticationDataSource();
+
+      const result = await auth.signIn(authDataSource, {
+        email: 'example@mail.com',
+        password: '123456',
+      });
+
+      expect(result).toStrictEqual({
+        data: null,
+        error: {
+          message: 'Credenciais inválidas',
+          fields: ['email', 'password'],
+        },
+      });
+    });
+
+    test('Providing invalid "password"', async () => {
+      const authDataSource = createAuthenticationDataSource();
+
+      const result = await auth.signIn(authDataSource, {
+        email: 'gabriel@email.com',
+        password: 'invalid',
+      });
+
+      expect(result).toStrictEqual({
+        data: null,
+        error: {
+          message: 'Credenciais inválidas',
+          fields: ['email', 'password'],
+        },
+      });
+    });
+
+    test('Providing correctly properties', async () => {
+      const authDataSource = createAuthenticationDataSource();
+
+      const result = await auth.signIn(authDataSource, {
+        email: 'gabriel@email.com',
+        password: '123456',
+      });
+
+      expect(result).toStrictEqual({
+        error: null,
+        data: {
+          accessToken: expect.any(String),
+        },
+      });
+
+      expect(result.data?.accessToken.split('.').length).toBe(3);
     });
   });
 });
---
+++ types/SignUp.ts
@@ -0,0 +1,7 @@
+export type SignUpProps = {
+  email: string;
+  name: string;
+  userName: string;
+  password: string;
+  confirmPassword: string;
+};
