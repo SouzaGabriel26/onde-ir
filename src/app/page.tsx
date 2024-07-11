4368859786714a7ab588a54c1a40206b68ba4706
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-07-11 20:08:34 -0300

    style(`home`): adds new style to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,63 @@
 import Link from 'next/link';
 
 import { Header } from '@/components/Header';
+import { ImageCard } from '@/components/ImageCard';
 import { Button } from '@/components/ui/Button';
 
-export default function Page() {
+import { sanitizeClassName } from '../utils/sanitizeClassName';
+
+export default async function Page() {
+  const mockedData = [
+    {
+      name: 'Café Bamboo',
+      country: 'Brasil',
+      state: 'ES',
+      image_url:
+        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-03.jpg?raw=true',
+    },
+    {
+      name: 'Churrascaria Espeto de Ouro',
+      country: 'Brasil',
+      state: 'ES',
+      image_url:
+        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-01.jpg?raw=true',
+    },
+    {
+      name: 'Restaurante do Porto',
+      country: 'Brasil',
+      state: 'ES',
+      image_url:
+        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-02.jpg?raw=true',
+    },
+    {
+      name: 'Madero Steak House',
+      country: 'Brasil',
+      state: 'ES',
+      image_url:
+        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-05.jpg?raw=true',
+    },
+  ];
+
   return (
-    <div className="relative flex min-h-screen flex-col overflow-hidden">
+    <div className="relative flex flex-col overflow-hidden lg:max-h-screen">
       <Header />
       <main
         className={`
+          mt-20
           flex
-          h-screen
           flex-1
           flex-col
           items-center
-          justify-center
-          gap-16
-          lg:grid
-          lg:grid-cols-3
-          lg:gap-36
-          lg:px-40
+          justify-evenly
+          gap-6
+          pt-2
+          lg:mt-10
+          lg:flex-row
+          lg:gap-0
+          lg:pt-0
         `}
       >
-        <div className="flex h-full flex-col items-center justify-center">
+        <section className="flex min-h-fit flex-1 items-center justify-center lg:max-w-fit">
           <div
             className={`
               flex
@@ -39,11 +74,11 @@ export default function Page() {
           >
             <h2
               className={`
-                text-title
                 w-fit
                 max-w-[320px]
                 text-5xl
                 font-semibold
+                text-primary
                 lg:min-w-[320px]
                 lg:text-7xl
               `}
@@ -51,7 +86,7 @@ export default function Page() {
               Viva uma grande experiência
             </h2>
 
-            <p className="text-paragraph max-w-64 lg:max-w-full">
+            <p className="max-w-64 text-muted-foreground lg:max-w-full">
               Descubra locais incríveis para se visitar em cidades maravilhosas.
             </p>
 
@@ -61,17 +96,50 @@ export default function Page() {
               </Link>
             </div>
           </div>
-        </div>
+        </section>
 
-        <div className="col-span-2 flex h-full flex-col items-center justify-center">
-          <div>
-            <p>fotos</p>
-            <p>fotos</p>
-            <p>fotos</p>
-            <p>fotos</p>
+        <section
+          className={`
+            flex
+            h-full
+            justify-center
+            px-8
+            lg:px-0
+          `}
+        >
+          <div
+            className={`
+              flex
+              flex-col
+              items-center
+              justify-center
+              gap-4
+              p-8
+              lg:grid
+              lg:min-h-screen
+              lg:grid-cols-2
+              lg:place-items-center
+              lg:gap-8
+            `}
+          >
+            {mockedData.map((place, index) => {
+              const description = `${place.country} - ${place.state}`;
+
+              return (
+                <ImageCard
+                  key={place.name}
+                  src={place.image_url}
+                  alt={place.name}
+                  description={description}
+                  title={place.name}
+                  className={sanitizeClassName(
+                    index === 1 || index === 3 ? 'lg:mt-2' : 'lg:mb-6',
+                  )}
+                />
+              );
+            })}
           </div>
-          <Link href="/dashboard">Dashboard</Link>
-        </div>
+        </section>
       </main>
     </div>
   );
