d0e945ac96e215ee6d9032491660705fcd1b4623
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-07-12 13:57:20 -0300

    style: style adjustments in homepage

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -24,45 +24,36 @@ export function ImageCard({
     <Link
       title={title}
       href="#"
-      className={sanitizeClassName(
-        `
-        rounded-[20px]
-        border
-        border-[#DCE2E5]
-        shadow
-        transition
-        hover:scale-105
-        hover:shadow-2xl
-      `,
-        className,
-      )}
+      className={sanitizeClassName('rounded-[20px] shadow', className)}
     >
-      <div
-        className={sanitizeClassName(
-          'relative',
-          variant === 'md' ? 'h-44 w-64' : 'h-52 w-72',
-        )}
-      >
-        <Image
-          fill
-          src={src}
-          alt={alt}
-          sizes="100%"
-          className="rounded-t-[20px] object-cover"
-        />
-      </div>
-      <div
-        className={sanitizeClassName(
-          `
+      <div className="rounded-[20px] border border-[#DCE2E5] transition hover:scale-105 hover:shadow-2xl">
+        <div
+          className={sanitizeClassName(
+            'relative',
+            variant === 'md' ? 'h-44 w-64' : 'h-52 w-72',
+          )}
+        >
+          <Image
+            fill
+            src={src}
+            alt={alt}
+            sizes="100%"
+            className="rounded-t-[20px] object-cover"
+          />
+        </div>
+        <div
+          className={sanitizeClassName(
+            `
           space-y-2
           rounded-b-[20px]
-          bg-white p-6
+          bg-white
+          p-6
           `,
-          variant === 'md' ? 'h-20' : 'h-28',
-        )}
-      >
-        <h3
-          className={`
+            variant === 'md' ? 'h-20' : 'h-28',
+          )}
+        >
+          <h3
+            className={`
             max-w-56
             overflow-hidden
             text-ellipsis
@@ -72,12 +63,13 @@ export function ImageCard({
             leading-5
             text-[#123952]
           `}
-        >
-          {title}
-        </h3>
-        <p className="text-paragraph leading-6 text-muted-foreground">
-          {description}
-        </p>
+          >
+            {title}
+          </h3>
+          <p className="text-paragraph leading-6 text-muted-foreground">
+            {description}
+          </p>
+        </div>
       </div>
     </Link>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,42 +4,44 @@ import { Header } from '@/components/Header';
 import { ImageCard } from '@/components/ImageCard';
 import { Button } from '@/components/ui/Button';
 
-import { sanitizeClassName } from '../utils/sanitizeClassName';
-
 export default async function Page() {
-  const mockedData = [
-    {
-      name: 'Café Bamboo',
-      country: 'Brasil',
-      state: 'ES',
-      image_url:
-        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-03.jpg?raw=true',
-    },
-    {
-      name: 'Churrascaria Espeto de Ouro',
-      country: 'Brasil',
-      state: 'ES',
-      image_url:
-        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-01.jpg?raw=true',
-    },
-    {
-      name: 'Restaurante do Porto',
-      country: 'Brasil',
-      state: 'ES',
-      image_url:
-        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-02.jpg?raw=true',
-    },
-    {
-      name: 'Madero Steak House',
-      country: 'Brasil',
-      state: 'ES',
-      image_url:
-        'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-05.jpg?raw=true',
-    },
-  ];
+  const mockedData = {
+    firstColumn: [
+      {
+        name: 'Café Bamboo',
+        country: 'Brasil',
+        state: 'ES',
+        image_url:
+          'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-03.jpg?raw=true',
+      },
+      {
+        name: 'Churrascaria Espeto de Ouro',
+        country: 'Brasil',
+        state: 'ES',
+        image_url:
+          'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-01.jpg?raw=true',
+      },
+    ],
+    secondColumn: [
+      {
+        name: 'Restaurante do Porto',
+        country: 'Brasil',
+        state: 'ES',
+        image_url:
+          'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-02.jpg?raw=true',
+      },
+      {
+        name: 'Madero Steak House',
+        country: 'Brasil',
+        state: 'ES',
+        image_url:
+          'https://github.com/SouzaGabriel26/onde-ir/blob/main/assets/photo-restaurant-05.jpg?raw=true',
+      },
+    ],
+  };
 
   return (
-    <div className="relative flex flex-col overflow-hidden lg:max-h-screen">
+    <div className="relative flex h-full flex-col overflow-hidden lg:max-h-screen">
       <Header />
       <main
         className={`
@@ -50,16 +52,15 @@ export default async function Page() {
           items-center
           justify-evenly
           gap-6
+          overflow-y-auto
           pt-2
-          lg:mt-10
           lg:flex-row
           lg:gap-0
           lg:pt-0
         `}
       >
-        <section className="flex min-h-fit flex-1 items-center justify-center lg:max-w-fit">
-          <div
-            className={`
+        <section
+          className={`
               flex
               max-w-[450px]
               flex-col
@@ -71,9 +72,9 @@ export default async function Page() {
               lg:max-w-[350px]
               lg:text-start
             `}
-          >
-            <h2
-              className={`
+        >
+          <h2
+            className={`
                 w-fit
                 max-w-[320px]
                 text-5xl
@@ -82,59 +83,49 @@ export default async function Page() {
                 lg:min-w-[320px]
                 lg:text-7xl
               `}
-            >
-              Viva uma grande experiência
-            </h2>
+          >
+            Viva uma grande experiência
+          </h2>
 
-            <p className="max-w-64 text-muted-foreground lg:max-w-full">
-              Descubra locais incríveis para se visitar em cidades maravilhosas.
-            </p>
+          <p className="max-w-64 text-muted-foreground lg:max-w-full">
+            Descubra locais incríveis para se visitar em cidades maravilhosas.
+          </p>
 
-            <div>
-              <Link href="#">
-                <Button size="lg">Descobrir todos lugares</Button>
-              </Link>
-            </div>
+          <div>
+            <Link href="#">
+              <Button size="lg">Descobrir todos lugares</Button>
+            </Link>
           </div>
         </section>
 
-        <section
-          className={`
-            flex
-            h-full
-            justify-center
-            px-8
-            lg:px-0
-          `}
-        >
-          <div
-            className={`
-              flex
-              flex-col
-              items-center
-              justify-center
-              gap-4
-              p-8
-              lg:grid
-              lg:min-h-screen
-              lg:grid-cols-2
-              lg:place-items-center
-              lg:gap-8
-            `}
-          >
-            {mockedData.map((place, index) => {
+        <section className="flex flex-col gap-8 py-4 lg:flex-row">
+          <div className="flex flex-col gap-8 first:mt-10">
+            {mockedData.firstColumn.map((place, index) => {
               const description = `${place.country} - ${place.state}`;
 
               return (
                 <ImageCard
-                  key={place.name}
-                  src={place.image_url}
-                  alt={place.name}
-                  description={description}
+                  key={`${index}-${place.name}`}
                   title={place.name}
-                  className={sanitizeClassName(
-                    index === 1 || index === 3 ? 'lg:mt-2' : 'lg:mb-6',
-                  )}
+                  description={description}
+                  alt={place.name}
+                  src={place.image_url}
+                />
+              );
+            })}
+          </div>
+
+          <div className="flex flex-col gap-8">
+            {mockedData.secondColumn.map((place, index) => {
+              const description = `${place.country} - ${place.state}`;
+
+              return (
+                <ImageCard
+                  key={`${index}-${place.name}`}
+                  title={place.name}
+                  description={description}
+                  alt={place.name}
+                  src={place.image_url}
                 />
               );
             })}
