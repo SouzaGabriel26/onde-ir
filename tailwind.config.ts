2eb42a84f3240e4adb80310f4ac2f3fc9522ae70
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-07-13 12:42:33 -0300

    style: add `fade-in` animation to images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,15 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'fade-in': {
+          from: { opacity: '0', scale: '0.70' },
+          to: { opacity: '1', scale: '1' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in': 'fade-in 0.4s ease-out',
       },
     },
   },
