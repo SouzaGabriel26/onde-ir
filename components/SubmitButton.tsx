662a9337083570f03b8d95bf80c3c311fdbb9bd0
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-07-16 18:33:43 -0300

    refactor(`SubmitButton`): refacts submit button component to use `useFormState`

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,19 +1,57 @@
 'use client';
 
-import { useFormStatus } from 'react-dom';
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
+import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/Button';
+import { Failure, Success } from '@/src/utils/operationResult';
+
+type ActionResponse = Promise<
+  Success<{ message: string; redirectLink: string }> | Failure<unknown>
+>;
 
 type SubmitButtonProps = {
   children: React.ReactNode;
   className?: string;
+  action?: (formData: FormData) => ActionResponse;
 };
 
-export function SubmitButton({ className, children }: SubmitButtonProps) {
+export function SubmitButton({
+  className,
+  children,
+  action,
+}: SubmitButtonProps) {
   const { pending } = useFormStatus();
 
+  const [state, formAction] = useFormState(
+    async (_prev: unknown, formData: FormData) => {
+      return await action?.(formData);
+    },
+    null,
+  );
+
+  useEffect(() => {
+    if (!state) return;
+
+    const { data, error } = state;
+
+    if (data) {
+      toast.success(data.message);
+      redirect(data.redirectLink);
+    } else {
+      toast.error(error.message);
+    }
+  }, [state]);
+
   return (
-    <Button disabled={pending} type="submit" className={className}>
+    <Button
+      type="submit"
+      disabled={pending}
+      className={className}
+      formAction={formAction}
+    >
       {children}
     </Button>
   );
