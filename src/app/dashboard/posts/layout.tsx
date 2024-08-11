382485e4b6f945b533b28cc7d61801eaebb40fad
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-08-11 02:36:54 -0300

    feat(`/dashboard/posts`): protect private `posts` page

--- src/app/dashboard/posts/layout.tsx
+++ src/app/dashboard/posts/layout.tsx
@@ -1,14 +1,32 @@
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
+import { headers } from 'next/headers';
 import Link from 'next/link';
+import { redirect, RedirectType } from 'next/navigation';
 import { ReactNode } from 'react';
 
 import { Button } from '@/components/ui/Button';
+import { createUserDataSource } from '@/data/user';
+import { user } from '@/models/user';
 
 type Props = {
   children: ReactNode;
 };
 
-export default function Layout({ children }: Props) {
+export default async function Layout({ children }: Props) {
+  const userId = headers().get('x-user-id') ?? '';
+
+  const userDataSource = createUserDataSource();
+  const { data } = await user.findById(userDataSource, {
+    id: userId,
+  });
+
+  if (!data) {
+    return redirect(
+      '/auth/signin?redirect_reason=not-authenticated',
+      RedirectType.replace,
+    );
+  }
+
   return (
     <section className="flex h-full flex-col space-y-10">
       <div>
