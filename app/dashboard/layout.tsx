257214e52b0ce61ea9e3368a003792050bfbcee1
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-09-07 20:11:40 -0300

    feat(`/dashboard`): lists places in a grid display

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,10 +11,10 @@ export default async function Layout({ children }: Props) {
   const { data } = await verify.loggedUser();
 
   return (
-    <main className="flex h-screen flex-col overflow-y-auto">
+    <main className="flex h-screen flex-col overflow-y-hidden">
       <Header userData={data} />
 
-      <div className="mt-16 h-full px-1 py-10 lg:mt-10 lg:px-40">
+      <div className="mt-16 h-full px-1 py-10 lg:mt-10 lg:px-12">
         {children}
       </div>
     </main>
