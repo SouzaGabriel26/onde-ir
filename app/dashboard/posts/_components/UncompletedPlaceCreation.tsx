b83446e11949c5c11f88a93bd906e39741525654
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-01-03 21:59:28 -0300

    feat: creates `UncompletedPlaceCreation` component

---
+++ app/dashboard/posts/_components/UncompletedPlaceCreation.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button } from '@/components/ui/Button';
+import { redirect } from 'next/navigation';
+import { dismissUncompletedPlaceCreationAction } from '../create/action/store';
+
+export type UncompletedPlaceCreationProps = {
+  placeId: string;
+  placeName: string;
+};
+
+export function UncompletedPlaceCreation({
+  placeId,
+  placeName,
+}: UncompletedPlaceCreationProps) {
+  return (
+    <div className="h-full w-full flex items-center justify-center flex-col gap-4">
+      <p className="text-center">
+        Você possui um post incompleto (não cadastrou as imagens): {placeName}
+      </p>
+      <div className="flex gap-4">
+        <Button
+          title={`Ignorar post incompleto de ${placeName}`}
+          variant="destructive"
+          onClick={dismissUncompletedPlaceCreationAction}
+        >
+          Descartar/Ignorar post incompleto
+        </Button>
+
+        <Button
+          title={`Cadastrar imagens para ${placeName}`}
+          onClick={() => {
+            redirect(`/dashboard/posts/${placeId}/create/images`);
+          }}
+        >
+          Cadastrar imagens
+        </Button>
+      </div>
+    </div>
+  );
+}
