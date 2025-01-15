33553b67ee1355d4490c507c58b2b96b6719377c
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-01-14 22:46:31 -0300

    refactor(utils/verify): adjusts `loggedUser` function to get token from cookies directly and verify

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,5 @@
 export const constants = {
   themeKey: 'onde-ir:theme',
   accessTokenKey: 'onde-ir:accessToken',
-  headerPayloadKey: 'x-user-id',
   uncompletedPlaceCreatedKey: 'onde-ir:uncompletedPlaceCreated',
 };
