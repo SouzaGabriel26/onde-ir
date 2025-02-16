1b6361a70cbcfe5b7bf90ebbdce1b5a2c69391b0
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-02-16 11:14:02 -0300

    chore: adjusts `places_comments` populate script to insert nested comments

--- infra/queries/05-populate-places-comments.sql
+++ infra/queries/05-populate-places-comments.sql
@@ -13,8 +13,32 @@ VALUES
     'Achei interessante este local, frequento bastante!'
   ),
   (
-    (SELECT id FROM places WHERE status = 'APPROVED' AND name = 'Café Bamboo'),
-    (SELECT id from users WHERE name = 'Normal user'),
+    (SELECT id FROM places WHERE status = 'APPROVED' AND name = 'Churrascaria Espeto de Ouro'),
+    (SELECT id FROM users WHERE name = 'Normal user'),
     NULL,
-    'Um ótimo café'
+    'Local muito bonito e aconchegante! Recomendo demais, vou todo mês com minha família.'
   );
+
+INSERT INTO place_comments
+  (
+    place_id,
+    user_id,
+    parent_comment_id,
+    description
+  )
+VALUES
+  (
+    (SELECT id FROM places WHERE status = 'APPROVED' AND name = 'Churrascaria Espeto de Ouro'),
+    (SELECT id FROM users WHERE name = 'Admin user'),
+    (SELECT id FROM place_comments WHERE user_id = (SELECT id FROM users WHERE name = 'Normal user')),
+    'Que legal!'
+  );
+
+
+
+
+
+
+
+
+
