2c7c47e8fedaf0f4ee36ac4180e4379701c9d87a
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-03-06 14:05:33 -0300

    feat: creates SQL `trigger` and `function` to pre-compute the value of `total comment likes count`

--- infra/queries/drop-schema-and-populate-tables.sql
+++ infra/queries/drop-schema-and-populate-tables.sql
@@ -77,6 +77,7 @@ CREATE TABLE place_comments (
   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   parent_comment_id UUID REFERENCES place_comments(id) ON DELETE CASCADE,
   description VARCHAR(255) NOT NULL,
+  likes_count INTEGER DEFAULT 0,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC')
 );
@@ -89,3 +90,27 @@ CREATE TABLE place_comment_likes (
 
   UNIQUE (comment_id, user_id)
 );
+
+-- Functions
+CREATE FUNCTION update_comment_likes_count() RETURNS TRIGGER AS $$
+  BEGIN
+    IF TG_OP = 'INSERT' THEN
+      UPDATE place_comments
+      SET likes_count = likes_count + 1
+      WHERE id = NEW.comment_id;
+    END IF;
+
+    IF TG_OP = 'DELETE' THEN
+      UPDATE place_comments
+      SET likes_count = likes_count - 1
+      WHERE id = OLD.comment_id;
+    END IF;
+
+    RETURN NULL;
+  END;
+$$ LANGUAGE plpgsql;
+
+-- Triggers
+CREATE OR REPLACE TRIGGER update_comment_likes_count_trigger
+AFTER INSERT OR DELETE ON place_comment_likes
+FOR EACH ROW EXECUTE FUNCTION update_comment_likes_count();
