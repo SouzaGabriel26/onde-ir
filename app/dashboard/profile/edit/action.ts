700d813b780e1549b3b76c1e04d4ac672f2d42a1
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-03-28 18:42:52 -0300

    feat: creates `updatePasswordAction` server action to handle `change password` feature

--- app/dashboard/profile/edit/action.ts
+++ app/dashboard/profile/edit/action.ts
@@ -1,7 +1,10 @@
 'use server';
 
+import { createAuthenticationDataSource } from '@/data/authentication';
 import { getPresignedURL, uploadFileToS3 } from '@/data/lambda';
+import { password } from '@/models/password';
 import type { User } from '@/types';
+import { feedbackMessage } from '@/utils/feedbackMessage';
 import { form } from '@/utils/form';
 import { verify } from '@/utils/verify';
 
@@ -105,4 +108,76 @@ export async function editUserAction(
   function checkIsToUpdatePassword() {
     return Boolean(data.current_password || data.new_password);
   }
+}
+
+type RawUpdatePasswordData = {
+  current_password: string;
+  new_password: string;
+  confirm_new_password: string;
+};
+
+type UpdatePasswordActionResponse = {
+  success: boolean;
+  error?: {
+    message: string;
+    errorFields?: Array<keyof RawUpdatePasswordData>;
+    fields?: {
+      current_password: string;
+      new_password: string;
+      confirm_new_password: string;
+    };
+  };
+};
+
+export async function updatePasswordAction(
+  _prevState: unknown,
+  formData: FormData,
+): Promise<UpdatePasswordActionResponse> {
+  const data = form.sanitizeData<RawUpdatePasswordData>(formData);
+  const { data: user } = await verify.loggedUser();
+
+  if (!user) {
+    return {
+      success: false,
+      error: {
+        message: 'Usuário não encontrado',
+      },
+    };
+  }
+
+  const authDataSource = createAuthenticationDataSource();
+  const changePasswordResult = await password.change(authDataSource, {
+    current_password: data.current_password,
+    new_password: data.new_password,
+    confirm_new_password: data.confirm_new_password,
+    user_id: user.id!,
+  });
+
+  if (changePasswordResult.error) {
+    await feedbackMessage.setFeedbackMessage({
+      type: 'error',
+      content: changePasswordResult.error.message,
+    });
+
+    return {
+      success: false,
+      error: {
+        message: changePasswordResult.error.message,
+        errorFields: changePasswordResult.error.fields as Array<
+          keyof RawUpdatePasswordData
+        >,
+        fields: {
+          current_password: data.current_password,
+          new_password: data.new_password,
+          confirm_new_password: data.confirm_new_password,
+        },
+      },
+    };
+  }
+
+  await feedbackMessage.setFeedbackMessage({
+    type: 'success',
+    content: 'Senha atualizada com sucesso!',
+  });
+  return { success: true };
 }
