21a690e8f7f51f3e2ee4986e162c9795525027e9
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-12 17:26:56 -0300

    feat: creates functionality of update user info in `models`

--- models/user.ts
+++ models/user.ts
@@ -6,6 +6,7 @@ import { validator } from './validator';
 
 export const user = Object.freeze({
   findById,
+  update,
 });
 
 export type FindByIdInput = {
@@ -37,4 +38,45 @@ async function findById(userDataSource: UserDataSource, input: FindByIdInput) {
   });
 
   return operationResult.success(foundUser);
+}
+
+export type UpdateUserInput = {
+  user_id: string;
+  email?: string;
+  name?: string;
+  user_name?: string;
+  avatar_url?: string;
+};
+
+async function update(userDataSource: UserDataSource, input: UpdateUserInput) {
+  const validatedInput = validator(
+    {
+      user_id: input.user_id,
+      email: input.email,
+      name: input.name,
+      user_name: input.user_name,
+      avatar_url: input.avatar_url,
+    },
+    {
+      user_id: 'required',
+      email: 'optional',
+      name: 'optional',
+      user_name: 'optional',
+      avatar_url: 'optional',
+    },
+  );
+
+  if (validatedInput.error) return validatedInput;
+
+  const { email, name, user_name, avatar_url, user_id } = validatedInput.data;
+
+  await userDataSource.update({
+    user_id,
+    email,
+    name,
+    user_name,
+    avatar_url,
+  });
+
+  return operationResult.success({});
 }
