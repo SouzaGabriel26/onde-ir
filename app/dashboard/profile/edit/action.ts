69a80acc8b73adea5ccc8531457e74c7bc756909
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-13 12:26:18 -0300

    feat: integrates functionality of edit user profile with frontend via server actions

--- app/dashboard/profile/edit/action.ts
+++ app/dashboard/profile/edit/action.ts
@@ -2,22 +2,31 @@
 
 import { createAuthenticationDataSource } from '@/data/authentication';
 import { getPresignedURL, uploadFileToS3 } from '@/data/lambda';
+import { createUserDataSource } from '@/data/user';
 import { password } from '@/models/password';
+import { user } from '@/models/user';
 import type { User } from '@/types';
 import { feedbackMessage } from '@/utils/feedbackMessage';
 import { form } from '@/utils/form';
 import { verify } from '@/utils/verify';
+import { revalidatePath } from 'next/cache';
+
+type EditUserRawData = {
+  email: string;
+  name: string;
+  username: string;
+  avatar_file?: File | null;
+};
 
 type EditUserActionResponse = {
   success: boolean;
   error?: {
     message: string;
+    errorFields?: Array<keyof EditUserRawData>;
     fields: {
       email: string;
       name: string;
-      user_name: string;
-      current_password: string;
-      new_password: string;
+      username: string;
     };
   };
 };
@@ -26,20 +35,11 @@ export async function editUserAction(
   _prevState: unknown,
   formData: FormData,
 ): Promise<EditUserActionResponse> {
-  const { data: user } = await verify.loggedUser();
+  const { data: loggedUser } = await verify.loggedUser();
 
-  interface RawData {
-    email: string;
-    name: string;
-    user_name: string;
-    current_password?: string;
-    new_password?: string;
-    avatar_file?: File | null;
-  }
+  const data = form.sanitizeData<EditUserRawData>(formData);
 
-  const data = form.sanitizeData<RawData>(formData);
-
-  if (!user) {
+  if (!loggedUser) {
     return {
       success: false,
       error: {
@@ -47,18 +47,13 @@ export async function editUserAction(
         fields: {
           email: data.email,
           name: data.name,
-          user_name: data.user_name,
-          current_password: '',
-          new_password: '',
+          username: data.username,
         },
       },
     };
   }
 
-  const isToUpdatePassword = checkIsToUpdatePassword();
-  // TODO: update password
-
-  let newAvatarUrl = '';
+  let newAvatarUrl: string | undefined = undefined;
   if (data.avatar_file?.size) {
     // upload to lambda and get url
     const { file_url, presigned_url } = await getPresignedURL(data.avatar_file);
@@ -67,46 +62,72 @@ export async function editUserAction(
     newAvatarUrl = file_url;
   }
 
-  console.log(newAvatarUrl);
+  const rawUserDataToUpdate = {
+    email: data.email,
+    name: data.name,
+    user_name: data.username,
+    avatar_url: newAvatarUrl,
+  };
 
-  const partialUserToUpdate: Partial<User> = {};
+  type RawUserDataKey = keyof typeof rawUserDataToUpdate;
 
-  for (const [key, value] of Object.entries(data)) {
-    if (isChanged(key as keyof RawData)) {
-      partialUserToUpdate[key as keyof User] = value;
+  let partialUserToUpdate: Partial<User> = {};
+
+  for (const [key, value] of Object.entries(rawUserDataToUpdate)) {
+    if (isChanged(key as RawUserDataKey)) {
+      partialUserToUpdate[key as RawUserDataKey] = value;
     }
   }
 
-  // TODO: Update partial user
-  const updatedData = {
-    ...partialUserToUpdate,
-    isToUpdatePassword,
-    newAvatarUrl: newAvatarUrl || undefined,
-  };
+  partialUserToUpdate = JSON.parse(JSON.stringify(partialUserToUpdate));
 
-  console.log(JSON.parse(JSON.stringify(updatedData, null, 2)));
+  if (!Object.keys(partialUserToUpdate).length) {
+    return {
+      success: false,
+      error: {
+        message: 'Nenhum dado foi alterado',
+        fields: {
+          email: data.email,
+          name: data.name,
+          username: data.username,
+        },
+      },
+    };
+  }
+
+  const userDataSource = createUserDataSource();
+  const { error } = await user.update(userDataSource, {
+    user_id: loggedUser.id!,
+    ...partialUserToUpdate,
+  });
+
+  if (error) {
+    return {
+      success: false,
+      error: {
+        message: error.message,
+        errorFields: error.errorFields as Array<keyof EditUserRawData>,
+        fields: {
+          email: data.email,
+          name: data.name,
+          username: data.username,
+        },
+      },
+    };
+  }
+
+  revalidatePath('/dashboard/profile/edit');
 
   return {
     success: true,
   };
 
-  function isChanged(currentField: keyof RawData) {
-    if (
-      currentField === 'current_password' ||
-      currentField === 'new_password'
-    ) {
-      return Boolean(data[currentField]);
-    }
-
-    if (user && user[currentField as keyof User] !== undefined) {
-      return data[currentField] !== user[currentField as keyof User];
+  function isChanged(currentField: RawUserDataKey) {
+    if (loggedUser && loggedUser[currentField] !== undefined) {
+      return rawUserDataToUpdate[currentField] !== loggedUser[currentField];
     }
 
     return false;
-  }
-
-  function checkIsToUpdatePassword() {
-    return Boolean(data.current_password || data.new_password);
   }
 }
 
