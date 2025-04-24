8c24c5006efe96f67d98139cbcb23e0696b6b81d
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-23 22:55:35 -0300

    refactor: adjusts page layouts padding

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,12 +12,12 @@ export default async function Layout({ children }: Props) {
   const { data } = await verify.loggedUser();
 
   return (
-    <main className="flex h-screen flex-col overflow-y-hidden">
-      <Header userData={data} />
+    <main className="flex h-screen flex-col overflow-y-hidden md:px-50">
+      <Header divider userData={data} />
 
       <div className="flex h-full justify-between">
         {data?.userRole === 'ADMIN' && <CmsSidebar />}
-        <div className="mt-10 h-full p-10 flex w-full justify-center pb-12">
+        <div className="mt-10 h-full flex w-full justify-center pb-12">
           {children}
         </div>
       </div>
