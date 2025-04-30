176a5b39af5dbef5823ea494576b41016caa4c82
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-30 16:14:14 -0300

    feat: add animation and `see-more` option to `PlacesFilters` tab navigation

--- app/dashboard/_components/PlacesFilters.tsx
+++ app/dashboard/_components/PlacesFilters.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import { Button } from '@/components/ui/Button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/Popover';
 import type { Category } from '@/data/place';
-import { sanitizeClassName } from '@/utils/sanitizeClassName';
+import { motion } from 'framer-motion';
+import { CheckIcon } from 'lucide-react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 
 type PlacesFiltersProps = {
   categories: Category[];
@@ -13,22 +21,46 @@ export function PlacesFilters({ categories }: PlacesFiltersProps) {
   const searchParams = useSearchParams();
   const currentFilter = searchParams.get('type');
 
+  const [hoveredTab, setHoveredTab] = useState<string>(currentFilter ?? 'all');
+
+  const [first, second, ...rest] = categories;
+  const firstTwoCatgories = [first, second];
+
+  const hiddenCategoryNames = rest.map(({ name }) => name);
+
   return (
     <nav className="flex justify-center">
-      <ul className="flex gap-4">
-        <li className="cursor-pointer">
+      <ul className="flex gap-2 bg-muted p-1 rounded-lg items-center text-xs md:text-base">
+        <motion.li
+          onHoverStart={() => setHoveredTab('all')}
+          onHoverEnd={() => setHoveredTab(currentFilter ?? 'all')}
+          className="cursor-pointer relative m-2"
+        >
           <Link
             href="/dashboard"
             as="/dashboard"
-            className={sanitizeClassName(
-              !currentFilter ? 'border-b-2 border-primary' : '',
-            )}
+            className=" rounded-[calc(var(--radius)-2px)]"
           >
             Todos
           </Link>
-        </li>
-        {categories.map(({ id, name }) => (
-          <li key={id} className="cursor-pointer">
+
+          {hoveredTab === 'all' && (
+            <motion.div
+              layoutId="underline"
+              className="absolute bottom-0 left-0 right-0 h-[2px] bg-primary"
+              transition={{ duration: 0.4 }}
+              exit={{ opacity: 0, transition: { duration: 0.4 } }}
+            />
+          )}
+        </motion.li>
+
+        {firstTwoCatgories.map(({ id, name }) => (
+          <motion.li
+            key={id}
+            onHoverStart={() => setHoveredTab(name)}
+            onHoverEnd={() => setHoveredTab(currentFilter ?? 'all')}
+            className="cursor-pointer relative m-2"
+          >
             <Link
               href={{
                 href: '/dashboard/',
@@ -36,15 +68,77 @@ export function PlacesFilters({ categories }: PlacesFiltersProps) {
                   type: name,
                 },
               }}
-              className={sanitizeClassName(
-                'capitalize',
-                currentFilter === name ? 'border-b-2 border-primary' : '',
-              )}
+              className="capitalize rounded-[calc(var(--radius)-2px)]"
             >
               {name}
             </Link>
-          </li>
+
+            {hoveredTab === name && (
+              <motion.div
+                layoutId="underline"
+                className="absolute bottom-0 left-0 right-0 h-[2px] bg-primary"
+                transition={{ duration: 0.4 }}
+                exit={{ opacity: 0, transition: { duration: 0.4 } }}
+              />
+            )}
+          </motion.li>
         ))}
+
+        <li>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="ghost" className="text-xs md:text-base px-2">
+                <motion.span
+                  onHoverStart={() => setHoveredTab('see-more')}
+                  onHoverEnd={() => setHoveredTab(currentFilter ?? 'all')}
+                  className="cursor-pointer relative"
+                >
+                  Ver mais
+                  {hoveredTab === 'see-more' ||
+                  hiddenCategoryNames.includes(currentFilter!) ? (
+                    <motion.div
+                      layoutId="underline"
+                      className="absolute bottom-0 left-0 right-0 h-[2px] bg-primary"
+                      transition={{ duration: 0.4 }}
+                      exit={{ opacity: 0, transition: { duration: 0.4 } }}
+                    />
+                  ) : null}
+                </motion.span>
+              </Button>
+            </PopoverTrigger>
+
+            <PopoverContent className="w-56 p-2">
+              <ul className="flex flex-col gap-2">
+                {rest.map(({ id, name }) => (
+                  <motion.li
+                    onHoverStart={() => setHoveredTab('see-more')}
+                    onHoverEnd={() => setHoveredTab(currentFilter ?? 'all')}
+                    key={id}
+                    className="cursor-pointer hover:bg-muted rounded-lg"
+                  >
+                    <Link
+                      href={{
+                        href: '/dashboard/',
+                        query: {
+                          type: name,
+                        },
+                      }}
+                      className={
+                        'capitalize flex items-center justify-between p-2 rounded-[calc(var(--radius)-2px)]'
+                      }
+                    >
+                      {name}
+
+                      {currentFilter === name && (
+                        <CheckIcon className="size-4" />
+                      )}
+                    </Link>
+                  </motion.li>
+                ))}
+              </ul>
+            </PopoverContent>
+          </Popover>
+        </li>
       </ul>
     </nav>
   );
